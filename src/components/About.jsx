6936276e1866704dc6938fe2adc8ef6567189c39
Tolgahan Acar <onquer_tolga@hotmail.com>
2023-06-16 12:34:17 +0300

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250] ">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card  "
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] max-w-[230px]  flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center ">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] "
+      >
+        I have met with programming at highschool and i realised that
+        programming is like a hobby for me. At all the times that i have free
+        time i found myself coding something. At those times i decided that
+        programming will be my career path. Programming is a passion for me. I
+        learned Fundamentals of object-oriented programming and procedural
+        programming followed by studying on being front-end developer. For that
+        purpose I learned HTML, CSS, Javascript then studied a few libraries for
+        developing better projects like React.Js and Node.Js and now i am able
+        to code responsive user interfaces according to UI and UX design
+        standards. Last a few months i have been studying on Data structures for
+        better understanding the working mechanism of most of the softwares and
+        design patterns like singleton and factory. My main purpose is being
+        part of the industry and becoming a well known programmer.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/components/note.js
@@ -0,0 +1,12 @@
+/* .send(
+        "service_z9518ah",
+        "template_hfh3kfp",
+        {
+          from_name: form.name,
+          to_name: "Tolgahan",
+          from_email: form.email,
+          to_email: "tolgahanacarrr@gmail.com",
+          message: form.message,
+        },
+        "XJqiZ4cQuBi3koyqk"
+      ) */
